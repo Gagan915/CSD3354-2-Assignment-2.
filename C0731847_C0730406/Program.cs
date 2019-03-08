419ebec274676a6987b8fd88813fa2c8662589e1
visualstudio <visualstudio@122-PC12>
2019-03-08 18:18:02 -0500

    Lab01

--- C0731847_C0730406/Program.cs
+++ C0731847_C0730406/Program.cs
@@ -13,5 +13,22 @@ namespace C0731847_C0730406
             //Gagandeep Kaur Waraich(C0731847)
             //Manpreet Kaur(C0730406)
         }
+        
+public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
     }
+
+
+
+
 }
+
