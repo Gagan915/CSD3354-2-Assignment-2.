7c15217996bf6002c5d49453868319cf4231251c
visualstudio <visualstudio@122-PC12>
2019-03-08 20:17:12 -0500

    Lab02

--- C0731847_C0730406/Program.cs
+++ C0731847_C0730406/Program.cs
@@ -8,24 +8,30 @@ namespace C0731847_C0730406 {
 //Gagandeep Kaur Waraich(C0731847)
 //Manpreet Kaur(C0730406)
  class Program {
-    public static void Main(string[] args)
+ static void Main(string[] args)
     {
         DelegateExercises mg = new DelegateExercises();
-        mg.Method2();
+        
+
+            mg.Method2(50);
     }
 }
 public class DelegateExercises
 {
-    public delegate void MyDelegate();
-    void Method1()
+  
+            public delegate void MyDelegate(int i);
+        public void Method1(int i)
     {
         Console.WriteLine("Method1");
+            Console.WriteLine(i);
         Console.ReadLine();
     }
-    public void Method2()
+ 
+     public void Method2(int i)
     {
         MyDelegate myDelegate = new MyDelegate(Method1);
-        myDelegate();
+   
+            myDelegate(50);
     }
 }
 }
