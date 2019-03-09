94f61a4ea208576be227d8ae016cfa516ebfed62
visualstudio <visualstudio@122-PC12>
2019-03-08 20:36:13 -0500

    Lab04

--- C0731847_C0730406/Program.cs
+++ C0731847_C0730406/Program.cs
@@ -4,36 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0731847_C0730406 { 
-//Gagandeep Kaur Waraich (C0731847)
-//Manpreet Kaur (C0730406)
- class Program {
- static void Main(string[] args)
-    {
-        DelegateExercises mg = new DelegateExercises();
-
-            mg.Method2(60);
-         
-    }
-}
-public class DelegateExercises
+namespace C0731847_C0730406
 {
-  
-            public delegate void MyDelegate(int i);
-        public void Method1(int i)
+    //Gagandeep Kaur Waraich (C0731847)
+    //Manpreet Kaur (C0730406)
+    class Program
     {
-        Console.WriteLine("Method1");
-            Console.WriteLine(i);
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            DelegateExercises mg = new DelegateExercises();
+
+            mg.Method3(10);
+            Console.Read();
+
+        }
     }
- 
-     public void Method2(int i)
+    public class DelegateExercises
     {
-        MyDelegate myDelegate = new MyDelegate(Method1);
-   
-            myDelegate(60);
+
+        public delegate int MyDelegate(int i);
+        public int Method1(int intMethod1)
+        {
+            return intMethod1 * 2;
+
+        }
+
+        public int Method2(int intMethod2)
+        {
+            return intMethod2 * 10;
+        }
+        public void Method3(int intMethod3)
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            int result1 = myDelegate(10);
+            System.Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            System.Console.WriteLine(result2);
+        }
     }
-}
 }
 
 
