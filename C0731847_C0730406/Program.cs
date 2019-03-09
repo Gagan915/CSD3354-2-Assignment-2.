0f489843ba8f4637c0d0828f05e1af9617e559c0
visualstudio <visualstudio@122-PC12>
2019-03-08 21:38:16 -0500

    Lab15

--- C0731847_C0730406/Program.cs
+++ C0731847_C0730406/Program.cs
@@ -6,34 +6,89 @@ using System.Threading.Tasks;
 
 namespace C0731847_C0730406
 {
-    using System;
+    public class Program
 
-    class Program
     {
-        public static void Main(string[] args)
+
+        public static void Main()
+
         {
-            DelegateExercises mg = new DelegateExercises();
-            mg.Method3();
-            Console.ReadLine();
+
+            DelegateExercises delegateExercises = new DelegateExercises();
+
+            try
+
+            {
+
+                delegateExercises.Method3();
+
+                Console.ReadLine();
+
+            }
+
+            catch (System.Exception ex)
+
+            {
+
+                System.Console.WriteLine("Exception Occurred.");
+
+                Console.ReadLine();
+
+                delegateExercises.Method3();
+
+                Console.ReadLine();
+
+
+
+            }
+
         }
+
     }
-    delegate int MyDelegate(out int i);
+
+
+
+
+
+    public delegate void MyDelegate(ref int intValue);
+
+
+
     public class DelegateExercises
+
     {
-        int Method1(out int i)
+
+
+
+        void Method1(ref int intValue)
+
         {
 
-            i = 100;
-            Console.WriteLine("Method 1 " + i);
-            return 0;
+            throw new System.Exception();
+
+            intValue = intValue + 5;
+
+            System.Console.WriteLine("Method1 " + intValue);
+
         }
+
+
+
         public void Method3()
+
         {
+
             MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = null;
+
+
+
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+
             MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intValue;
-            myDelegate2(out intValue);
+
+            int intParameter = 5;
+
+            myDelegate2(ref intParameter);
 
         }
     }
