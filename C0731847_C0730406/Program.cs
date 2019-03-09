5958d5a21a028ac7ebe42b3b1f3ecd709bd02b95
visualstudio <visualstudio@122-PC12>
2019-03-08 19:57:46 -0500

    final lab01

--- C0731847_C0730406/Program.cs
+++ C0731847_C0730406/Program.cs
@@ -4,31 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0731847_C0730406
-{
-    class Program
+namespace C0731847_C0730406 { 
+//Gagandeep Kaur Waraich(C0731847)
+//Manpreet Kaur(C0730406)
+ class Program {
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //Gagandeep Kaur Waraich(C0731847)
-            //Manpreet Kaur(C0730406)
-        }
-        
-public delegate void MyDelegate();
-        void Method1()
-        {
-            Console.WriteLine("Method1");
-            Console.ReadLine();
-        }
-        public void Method2()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
+        DelegateExercises mg = new DelegateExercises();
+        mg.Method2();
     }
-
-
-
-
 }
+public class DelegateExercises
+{
+    public delegate void MyDelegate();
+    void Method1()
+    {
+        Console.WriteLine("Method1");
+        Console.ReadLine();
+    }
+    public void Method2()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        myDelegate();
+    }
+}
+}
+
 
